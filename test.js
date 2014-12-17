35f033b00c0e1227f2a27beb8ac7365a9093919f
Lars Wassermann <lars.wassermann@gmail.com>
2014-12-17 16:00:28 +0100

    fixed some of the issues with tests first test case for player score counting (no spares/strikes yet) rendering test results if wanted to

--- test.js
+++ test.js
@@ -1,23 +1,20 @@
-eTest = function (){
+Test = function (){
 }
 
-Object.extend(Test, {
+Object.extend(Test.prototype, {
 	assert: function(value, message) {
 		if (!!value) { return; }
 		throw new Error(message);
 	},
 	deny: function(value, message) {
 		return this.assert(!value, message);
-	};
-})
-
-Object.extend(Test, {
+	},
 	run: function() {
 		var result = {success: 0, failed: 0, errors:[]},
-			testCase = new this();
+			testCase = this;
 		// ToDo: look up property collection along the prototype chain
-		var testNames = Object.keys(this).filter(function(ea) {
-			return ea.startsWith('test');
+		var testNames = Object.keys(this.__proto__).filter(function(ea) {
+			return ea.indexOf('test') == 0;
 		});
 		testNames.forEach(function(ea) {
 			try {
@@ -33,6 +30,31 @@ Object.extend(Test, {
 				testCase.tearDown && testCase.tearDown();
 			}
 		})
+		return result;
 	}
 })
+
+PlayerTest = function() {};
+
+PlayerTest.prototype = new Test();
+Object.extend(PlayerTest.prototype, {
+	testScoring: function() {
+		var p = new bowling.Player();
+		this.assert(p.score() == 0);
+		p.frames[0][1] = 5;
+		this.assert(p.score() == 5);
+	}
+});
+
+
+// actually running the tests
+if (document.documentURI.indexOf('test=true') !== -1) {
+	var resultsVisualization = document.createElement('testResults'),
+		// beware premature generalization:
+		//     stick with one test case class for the moment
+		result = new PlayerTest().run();
+
+	resultsVisualization.innerHTML = result.success + ':' + result.failed;
+	document.body.appendChild(resultsVisualization);
+}
 
